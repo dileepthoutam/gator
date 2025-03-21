10231426260d836d04b3dafb511c098af1d5ab9b
dileepthoutam <thoutamdileep@gmail.com>
2025-03-21 20:31:58 +0530

    Formatted code with go fmt

--- main.go
+++ main.go
@@ -9,62 +9,61 @@ import (
 )
 
 type state struct {
-    config *config.Config
+	config *config.Config
 }
 type command struct {
-    name string
-    args []string
+	name string
+	args []string
 }
 
 type commands struct {
-    Command map[string]func(*state, command) error
+	Command map[string]func(*state, command) error
 }
 
 func (c *commands) register(name string, f func(*state, command) error) {
-    c.Command[name] = f
+	c.Command[name] = f
 }
 
 func (c *commands) run(s *state, cmd command) error {
-    if val, ok := c.Command[cmd.name]; ok {
-	return val(s, cmd)
-    }
-    return fmt.Errorf("Command doesn't exist")
+	if val, ok := c.Command[cmd.name]; ok {
+		return val(s, cmd)
+	}
+	return fmt.Errorf("Command doesn't exist")
 }
 
 func handlerLogin(s *state, cmd command) error {
-    if len(cmd.args) != 1 {
-	fmt.Println(fmt.Errorf("Username is required or you passed too many args."))
-	os.Exit(1)
-    }
-    username := cmd.args[0]
-    s.config.SetUser(username)
-    log.Printf("The user: %s has been set.", username)
-    return nil
+	if len(cmd.args) != 1 {
+		fmt.Println(fmt.Errorf("Username is required or you passed too many args."))
+		os.Exit(1)
+	}
+	username := cmd.args[0]
+	s.config.SetUser(username)
+	log.Printf("The user: %s has been set.", username)
+	return nil
 }
 
 func main() {
 
-    st := &state{
-	config: config.Read(),
-    }
+	st := &state{
+		config: config.Read(),
+	}
 
-    cmds := &commands{
-	Command: map[string]func(*state, command) error {
-	    "login": handlerLogin,
-	},
-    }
+	cmds := &commands{
+		Command: map[string]func(*state, command) error{
+			"login": handlerLogin,
+		},
+	}
 
-    if len(os.Args) < 2 {
-	log.Println("Atleast pass one argument")
-	os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		log.Println("Atleast pass one argument")
+		os.Exit(1)
+	}
 
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
 
-    cmd := command {
-	name: os.Args[1],
-	args: os.Args[2:],
-    }
-
-    cmds.run(st, cmd)
+	cmds.run(st, cmd)
 
 }
